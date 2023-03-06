8385f86efee8f438386f4ccdbe87e7d2e6b192fa
Nicolas Bonnefoie <team.devparis@gmail.com>
2023-03-06 01:45:13 +0100

    Implentation de la views

---
+++ controllers/__init__.py
@@ -0,0 +1,5 @@
+"""
+    Le fichier __init_ permet l'exportation,
+    de tout les fichiers des modules dans lesquel le fichier __init__ a ete cree.
+"""
+
---
+++ views/__init__.py
@@ -0,0 +1,4 @@
+"""
+    Le fichier __init_ permet l'exportation,
+    de tout les fichiers des modules dans lesquel le fichier __init__ a ete cree.
+"""
